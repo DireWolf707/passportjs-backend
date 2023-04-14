4aac22612074154fd10ad9598fc019be1274e7e1
DireWolf707 <rahulsood707@gmail.com>
2023-04-14 18:09:01 +0530

    commented unused error handlers

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,15 +1,15 @@
 import AppError from "./../utils/appError.js"
 
-const handleCastErrorDB = (err) => {
-  const message = `Invalid ${err.path}: ${err.value}.`
-  return new AppError(message, 400)
-}
+// const handleCastErrorDB = (err) => {
+//   const message = `Invalid ${err.path}: ${err.value}.`
+//   return new AppError(message, 400)
+// }
 
-const handleDuplicateFieldsDB = (err) => {
-  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0]
-  const message = `Duplicate field value: ${value}. Please use another value!`
-  return new AppError(message, 400)
-}
+// const handleDuplicateFieldsDB = (err) => {
+//   const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0]
+//   const message = `${value} is already in use`
+//   return new AppError(message, 400)
+// }
 
 const handleValidationErrorDB = (err) => {
   const errors = Object.values(err.errors).map(({ path, message }) => `${path}:${message}`)
@@ -37,8 +37,8 @@ const sendError = (err, req, res) => {
 export default (err, req, res, next) => {
   // Mongo Errors
   if (err.name === "ValidationError") err = handleValidationErrorDB(err)
-  if (err.name === "CastError") err = handleCastErrorDB(err)
-  if (err.code === 11000) err = handleDuplicateFieldsDB(err)
+  // if (err.code === 11000) err = handleDuplicateFieldsDB(err)
+  // if (err.name === "CastError") err = handleCastErrorDB(err)
 
   sendError(err, req, res)
 }
