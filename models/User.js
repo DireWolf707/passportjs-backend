f216f34002b8bc333595831a0b9ce68474b77740
DireWolf707 <rahulsood707@gmail.com>
2023-04-14 18:03:13 +0530

    added validation and err msg in schema

--- models/User.js
+++ models/User.js
@@ -3,20 +3,20 @@ import mongoose from "mongoose"
 const schema = new mongoose.Schema({
   email: {
     type: String,
-    required: true,
+    required: [true, "Email is required"],
     unique: true,
   },
   username: {
     type: String,
-    required: true,
+    required: [true, "Username is required"],
     trim: true,
-    minLength: 1,
+    minLength: [2, "Username should be of min length 2"],
   },
   name: {
     type: String,
-    required: true,
+    required: [true, "Name is required"],
     trim: true,
-    minLength: 1,
+    minLength: [2, "Name should be of min length 2"],
   },
   avatar: {
     type: String,
