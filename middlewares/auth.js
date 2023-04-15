91b367cd2f1b6798e071c0a2c9ad00d34fe41f6e
DireWolf707 <rahulsood707@gmail.com>
2023-04-15 21:17:05 +0530

    fixed isAuthenticated middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,6 @@
 import AppError from "../utils/appError.js"
 
 export const isAuthenticated = (req, res, next) => {
-  if (!req.user) throw new AppError("Not Authorized", 403)
-  next()
+  if (!req.user) next(new AppError("Not Authorized", 403))
+  else next()
 }
